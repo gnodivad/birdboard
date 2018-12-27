1614abbc7e414df81793b88ac853620c82ebf029
David <dodwei.dev@gmail.com>
2018-12-27 13:43:23 +0800

    Add test for projects list

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function store()
+    {
+        Project::create(request(['title', 'description']));
+
+        return redirect('/projects');
+    }
+}
