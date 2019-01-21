6ed4707362e412bcac6e3b6a68d15c650211d2b0
David <dodwei.dev@gmail.com>
2019-01-21 12:53:32 +0800

    A project can have task

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -16,10 +16,9 @@
                 <div class="mb-8">
                     <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
 
-                    <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card">Lorem ipsum</div>
+                    @foreach($project->tasks as $task)
+                        <div class="card mb-3">{{ $task->body }}</div>
+                    @endforeach
                 </div>
 
                 <div>
