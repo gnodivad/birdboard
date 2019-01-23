8ddbd69f12857da80e09fbea1de0d7fc94c414ed
David <dodwei.dev@gmail.com>
2019-01-23 13:41:13 +0800

    Persist project notes and add policy

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -42,7 +42,19 @@
                 <div>
                     <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
 
-                    <textarea class="card w-full" style="min-height: 200px">Lorem ipsum</textarea>
+                    <form method="POST" action="{{ $project->path() }}">
+                        @csrf
+                        @method('PATCH')
+
+                        <textarea
+                            name="notes"
+                            class="card w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
+                        <button type="submit" class="button">Save</button>
+                    </form>
                 </div>
             </div>
 
