a985dd209e56156afb257185f111c5336b3a0c4b
David <dodwei.dev@gmail.com>
2019-01-23 13:08:41 +0800

    Sort projects by updated timestamp

--- app/User.php
+++ app/User.php
@@ -33,6 +33,6 @@ class User extends Authenticatable
 
     public function projects()
     {
-        return $this->hasMany(Project::class, 'owner_id');
+        return $this->hasMany(Project::class, 'owner_id')->latest('updated_at');
     }
 }
