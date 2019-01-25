814654b4147f35b5ffe0d78ff32de895e93d2504
David <dodwei.dev@gmail.com>
2019-01-25 16:16:36 +0800

    project activity feeds

---
+++ tests/Feature/ActivityFeedTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Facades\Tests\Setup\ProjectFactory;
+use Tests\TestCase;
+
+class ActivityFeedTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function creating_a_project_generates_activity()
+    {
+        $project = ProjectFactory::create();
+
+        $this->assertCount(1, $project->activity);
+        $this->assertEquals('created', $project->activity[0]->description);
+    }
+
+    /** @test */
+    public function updating_a_project_generates_activity()
+    {
+        $project = ProjectFactory::create();
+
+        $project->update(['title' => 'Changed']);
+
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('updated', $project->activity->last()->description);
+    }
+}
