b8db0d760b10ea02c331a3bbf921419835b22af8
David <dodwei.dev@gmail.com>
2019-02-01 16:33:48 +0800

    Activity feed clean up

--- app/Task.php
+++ app/Task.php
@@ -17,10 +17,6 @@ class Task extends Model
     protected static function boot()
     {
         parent::boot();
-
-        static::created(function ($task) {
-            $task->project->recordActivity('created_task');
-        });
     }
 
     public function complete()
@@ -33,6 +29,8 @@ class Task extends Model
     public function incomplete()
     {
         $this->update(['completed' => false]);
+
+        $this->project->recordActivity('incompleted_task');
     }
 
     public function project()
