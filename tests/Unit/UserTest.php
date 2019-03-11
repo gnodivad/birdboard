52642074cdf603f4542c05799c2d0474192688f4
David <dodwei.dev@gmail.com>
2019-03-11 15:55:43 +0800

    Beware false positives

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Collection;
 use Tests\TestCase;
+use Facades\Tests\Setup\ProjectFactory;
 
 class UserTest extends TestCase
 {
@@ -17,5 +19,27 @@ class UserTest extends TestCase
 
         $this->assertInstanceOf(Collection::class, $user->projects);
     }
+
+    /** @test */
+    public function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = factory(User::class)->create();
+        $nick = factory(User::class)->create();
+
+        $project = tap(ProjectFactory::ownedBy($sally)->create())->invite($nick);
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
+    }
+
 
 }
