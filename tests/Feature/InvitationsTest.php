1a8e5e196f659f8967801c992038fcfa03596b33
David <dodwei.dev@gmail.com>
2019-03-15 15:51:44 +0800

    Inviting users as a feature test

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Facades\Tests\Setup\ProjectFactory;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class InvitationsTest extends TestCase
@@ -12,7 +12,45 @@ class InvitationsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function a_project_can_invite_a_user()
+    public function non_owners_may_not_invite_users()
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->post(ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    public function a_project_owner_can_invite_a_user()
+    {
+        $project = ProjectFactory::create();
+
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => $userToInvite->email
+            ])
+            ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    public function the_email_address_must_be_associated_with_a_valid_birdboard_account()
+    {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => 'notuser@example.com'
+            ])
+            ->assertSessionHasErrors([
+                'email' => 'The user you are inviting must have a Birdboard account.'
+            ]);
+    }
+
+    /** @test */
+    public function invited_users_may_update_project_details()
     {
         $project = ProjectFactory::create();
 
