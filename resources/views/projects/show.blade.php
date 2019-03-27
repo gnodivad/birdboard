edc657516a3b903a773dc95806e9a196e4a2b8c8
Jeffrey Way <jeffrey@jeffrey-way.com>
2019-03-27 15:30:33 -0400

    Cherry pick from remote

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,11 +1,11 @@
 @extends ('layouts.app')
 @section('content')
-<header class="flex items-center mb-3 pb-4">
-    <div class="flex justify-between items-end w-full">
-        <p class="text-default text-sm font-normal">
-            <a href="/projects" class="text-default text-sm font-normal no-underline hover:underline">My Projects</a> / {{
-            $project->title }}
-        </p>
+    <header class="flex items-center mb-6 pb-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-muted font-light">
+                <a href="/projects" class="text-muted no-underline hover:underline">My Projects</a>
+                / {{ $project->title }}
+            </p>
 
         <div class="flex items-center">
             @foreach($project->members as $member)
@@ -15,34 +15,54 @@
 
             <a href="{{ $project->path() . '/edit' }}" class="button ml-4">Edit Project</a>
         </div>
-    </div>
-</header>
+    </header>
 
-<main>
-    <div class="lg:flex -mx-3">
-        <div class="lg:w-3/4 px-3 mb-6">
-            <div class="mb-8">
-                <h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-lg text-muted font-light mb-3">Tasks</h2>
 
-                {{-- tasks --}} @foreach ($project->tasks as $task)
-                <div class="card mb-3">
-                    <form method="POST" action="{{ $task->path() }}">
-                        @method('PATCH') @csrf
+                    {{-- tasks --}}
+                    @foreach ($project->tasks as $task)
+                        <div class="card mb-3">
+                            <form method="POST" action="{{ $task->path() }}">
+                                @method('PATCH')
+                                @csrf
 
-                        <div class="flex">
-                            <input name="body" value="{{ $task->body }}" class="bg-card text-default w-full {{ $task->completed ? 'text-default' : '' }}">
-                            <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked'
-                            : '' }}>
+                                <div class="flex items-center">
+                                    <input name="body" value="{{ $task->body }}" class="text-default bg-card w-full {{ $task->completed ? 'line-through text-muted' : '' }}">
+                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    @endforeach
+
+                    <div class="card mb-3">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                            @csrf
+
+                            <input placeholder="Add a new task..." class="text-default bg-card w-full" name="body">
+                        </form>
+                    </div>
                 </div>
                 @endforeach
 
-                <div class="card mb-3">
-                    <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                <div>
+                    <h2 class="text-lg text-muted font-light mb-3">General Notes</h2>
+
+                    {{-- general notes --}}
+                    <form method="POST" action="{{ $project->path() }}">
                         @csrf
 
-                        <input placeholder="Add a new task..." class="bg-card text-default w-full" name="body">
+                        <textarea
+                            name="notes"
+                            class="card text-default w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
+                        <button type="submit" class="button">Save</button>
                     </form>
                 </div>
             </div>
