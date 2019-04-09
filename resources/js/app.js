c129404512a882c6cb14cb7d05bd42575931f202
David <dodwei.dev@gmail.com>
2019-04-09 15:16:10 +0800

    The new project modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,15 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+
+import VModal from "vue-js-modal";
+Vue.use(VModal);
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +22,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').default);
+Vue.component(
+    "theme-switcher",
+    require("./components/ThemeSwitcher.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +34,5 @@ Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').defaul
  */
 
 const app = new Vue({
-    el: '#app'
+    el: "#app"
 });
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -18,4 +18,40 @@
             <div>No projects yet.</div>
         @endforelse
     </main>
+
+    <modal name="hello-world" classes="p-10 bg-card rounded-lg" height="auto">
+        <h1 class="font-normal mb-16 text-center text-2xl">Let's Start Something New</h1>
+
+        <div class="flex">
+            <div class="flex-1 mr-4">
+                <div class="mb-4">
+                    <label for="title" class="text-sm block mb-2">Title</label>
+                    <input type="text" id="title" class="border border-muted-light p-2 text-xs block w-full rounded">
+                </div>
+
+                <div class="mb-4">
+                    <label for="description" class="text-sm block mb-2">Description</label>
+                    <textarea id="description" class="border border-muted-light p-2 text-xs block w-full rounded" rows=7></textarea>
+                </div>
+            </div>
+
+            <div class="flex-1 ml-4">
+                <div class="mb-4">
+                    <label class="text-sm block mb-2">Need Some Tasks?</label>
+                    <input type="text" class="border border-muted-light p-2 text-xs block w-full rounded" placeholder="Task 1">
+                </div>
+
+                <button class="inline-flex items-center text-xs">
+                    <span>Add New Task Field</span>
+                </button>
+            </div>
+        </div>
+
+        <footer class="flex justify-end">
+            <button class="button is-outlined mr-4">Cancel</button>
+            <button class="button">Create Project</button>
+        </footer>
+    </modal>
+
+    <a href="" @click.prevent="$modal.show('hello-world')">Show Modal</a>
 @endsection
