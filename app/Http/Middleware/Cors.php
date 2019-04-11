957997cdc5a1289cf5fcceb054a789e183ca45ae
David <dodwei.dev@gmail.com>
2019-04-11 16:34:11 +0800

    Add sample api for assets tutorial

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://assets.local')
+            ->header('Access-Control-Allow-Headers', 'x-requested-with');
+    }
+}
