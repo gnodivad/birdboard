4c1641e59d7454598bb83043e52ea1a2cc479384
David <dodwei.dev@gmail.com>
2019-04-22 16:00:46 +0800

    Hashed api tokens

--- app/User.php
+++ app/User.php
@@ -7,50 +7,51 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-	use Notifiable;
+    use Notifiable;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'name',
-		'email',
-		'password',
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'api_token'
+    ];
 
-	/**
-	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
-	 */
-	protected $hidden = [
-		'password',
-		'remember_token',
-	];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-	/**
-	 * Get all projects owner by the user.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
-	 */
-	public function projects()
-	{
-		return $this->hasMany(Project::class, 'owner_id')->latest('updated_at');
-	}
+    /**
+     * Get all projects owner by the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'owner_id')->latest('updated_at');
+    }
 
     /**
      * Get all projects that the user has access to.
-     * 
+     *
      * @return mixed
      */
-	public function accessibleProjects()
-	{
-	    return Project::where('owner_id', $this->id)
+    public function accessibleProjects()
+    {
+        return Project::where('owner_id', $this->id)
             ->orWhereHas('members', function ($query) {
                 $query->where('user_id', $this->id);
             })
             ->get();
-	}
+    }
 }
